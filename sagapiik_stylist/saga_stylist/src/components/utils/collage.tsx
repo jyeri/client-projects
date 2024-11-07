39aeba0bb54e104d8592e97803c5c5e514514ee5
jrummuka <jrummuka@student.hive.fi>
2024-11-07 13:48:56 +0200

    collage title margins

--- sagapiik_stylist/saga_stylist/src/components/utils/collage.tsx
+++ sagapiik_stylist/saga_stylist/src/components/utils/collage.tsx
@@ -81,7 +81,7 @@ export const Collage = ({ images, metadata }: CollageProps) => {
         <section className="relative flex h-[calc(100svh-var(--header-height))] snap-center items-center justify-center">
             <Container className="relative flex h-full max-h-[100svh] w-full flex-col items-center justify-center">
                 {metadata?.title && (
-                    <h2 className="relative mt-2 bottom-2 w-full p-2 text-center font-headers text-base font-bold tracking-wider text-textBlack sm:bottom-4 sm:text-lg md:text-xl lg:text-2xl">
+                    <h2 className="relative mt-[50px] w-full text-center font-headers text-base font-bold tracking-wider text-textBlack sm:bottom-4 sm:text-lg md:text-xl lg:text-2xl">
                         {metadata.title}
                     </h2>
                 )}
